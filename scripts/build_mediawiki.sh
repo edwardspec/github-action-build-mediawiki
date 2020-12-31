f672e9cc1b309d00c8b08302b0d52b629b4caf44
Edward Chernenko <edwardspec@gmail.com>
2020-12-31 23:43:06 +0300

    Use third-party action to install PHP/Composer of needed versions

--- scripts/build_mediawiki.sh
+++ scripts/build_mediawiki.sh
@@ -11,10 +11,6 @@ mkdir -p buildcache/mediawiki
 
 if [ ! -f buildcache/mediawiki/COMPLETE ]; then
 	(
-		# Downgrade PHP to 7.4 (GitHub Actions environment has PHP 8.0 preinstalled? not compatible with MW for now)
-		# TODO: should be able to select PHP version as parameter of GitHub action.
-		sudo apt update && DEBIAN_FRONTEND=noninteractive sudo apt-get install -y php7.4
-
 		cd buildcache
 		rm -rf mediawiki
 		git clone $GITCLONE_OPTS https://gerrit.wikimedia.org/r/p/mediawiki/core.git mediawiki
@@ -23,7 +19,6 @@ if [ ! -f buildcache/mediawiki/COMPLETE ]; then
 
 		find . -name .git | xargs rm -rf
 
-		sudo composer self-update --1 # wikimedia/composer-merge-plugin is not yet compatible with Composer 2.
 		composer install --no-interaction
 		touch COMPLETE # Mark this buildcache as usable
 	)

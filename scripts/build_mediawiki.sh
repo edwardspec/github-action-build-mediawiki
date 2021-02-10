3b774d837461055b7f7d03c1f8237f63af6016e5
Edward Chernenko <edwardspec@gmail.com>
2021-02-10 19:35:45 +0300

    Add missing clonebranch() method to scripts/build_mediawiki.sh

--- scripts/build_mediawiki.sh
+++ scripts/build_mediawiki.sh
@@ -7,6 +7,34 @@
 branch=$1
 GITCLONE_OPTS="--depth 1 --recurse-submodules -j 5 -b $branch"
 
+# Clone extension/skin of the chosen REL_ branch (if exists).
+# If REL_ branch doesn't exist (which happens when testing against trunk
+# or pre-release of MediaWiki core), "master" branch is used instead.
+#
+function clonebranch()
+{
+	# Shorter Travis logs: hide everything except "git clone" command
+	{ set +x; } 2>/dev/null
+
+	URL=$1
+	TARGET_DIRECTORY=$2
+	REL_BRANCH=$branch
+
+	# Skip if already cloned (e.g. if this extension is shipped with MediaWiki core)
+	[[ -d $TARGET_DIRECTORY ]] && return 0
+
+	# Check if REL_ branch is available. If not, use "master" as fallback.
+	git ls-remote --heads --exit-code $URL $REL_BRANCH >/dev/null || {
+		echo "NOTE: can't find $REL_BRANCH branch for $TARGET_DIRECTORY, using master branch instead." >&2
+		REL_BRANCH=master
+	}
+
+	set -x
+
+	# Download the sources
+	git clone -b $REL_BRANCH $GITCLONE_OPTS $URL $TARGET_DIRECTORY
+}
+
 mkdir -p buildcache/mediawiki
 
 if [ ! -f buildcache/mediawiki/COMPLETE ]; then
